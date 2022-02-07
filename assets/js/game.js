0112e606f890db52d79a3f9bbc25ba78d8f99f87
CarlBush <Carl.BushS13@gmail.com>
2022-02-07 14:36:30 -0700

    Console Log Created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,16 @@
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of qutation marks around playerName
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math
+console.log(10+10);
+//what is this?
+console.log("Our robort's name is" + playerName)
+
 // This creates a function named "fight"
 
 function fight() {
     window.alert("The fight has begun!");
 }
 
-fight();
+//fight();
