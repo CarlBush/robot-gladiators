e8696fb2090aa6bda89e27d45f616ed31f454e9d
CarlBush <Carl.BushS13@gmail.com>
2022-02-08 16:52:51 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ['Roborto', 'Amy Andrioid', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyNames) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -24,20 +21,20 @@ var fight = function() {
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
     // check enemy's HP
     if (enemyHealth<=0){
-        window.alert(enemyName + " has died! RIP");
+        window.alert(enemyNames + " has died! RIP");
     }
     else{
-        window.alert(enemyName + "still has " + enemyHealth + " health left.")
+        window.alert(enemyNames + "still has " + enemyHealth + " health left.")
     }
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack
     // Log a resulting message to the console so we know that it worked.
     console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
     //check OUR robot's HP
     if(playerHealth<=0){
@@ -70,5 +67,7 @@ var fight = function() {
     }
   };
 
-  fight();
+  for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+};
 
